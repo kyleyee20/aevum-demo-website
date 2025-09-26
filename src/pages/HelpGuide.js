539e43da81d574261360a4f391dcd49815d1bc6d
Kyle <kyleyee7546@gmail.com>
2025-09-25 23:45:10 -0700

    updated help pictures

--- src/pages/HelpGuide.js
+++ src/pages/HelpGuide.js
@@ -1,33 +1,48 @@
 import { useState } from "react";
 
 export default function HelpGuide() {
+    // Array of image paths in public/photos
     const images = [
-        "/public/photos/step1.png",
-        "/public/photos/step2.png",
-        "/public/photos/step3.png",
-        "/public/photos/step4.png",
-        "/public/photos/step5.png",
+        "/pictures/step1.png",
+        "/pictures/step2.png",
+        "/pictures/step3.png",
+        "/pictures/step4.png",
+        "/pictures/step5.png",
     ];
 
     const [current, setCurrent] = useState(0);
 
+    // Go to next image
     const nextImage = () => setCurrent((prev) => (prev + 1) % images.length);
-    const prevImage = () => setCurrent((prev) => (prev - 1 + images.length) % images.length);
+
+    // Go to previous image
+    const prevImage = () =>
+        setCurrent((prev) => (prev - 1 + images.length) % images.length);
 
     return (
         <div className="help-container">
             <h1>Help Guide</h1>
+
             <div className="help-card">
                 <div className="help-carousel">
-                    <button className="help-arrow" onClick={prevImage}>⬅</button>
+                    <button className="help-arrow" onClick={prevImage}>
+                        ⬅
+                    </button>
+
                     <img
                         className="help-image"
                         src={images[current]}
                         alt={`Step ${current + 1}`}
                     />
-                    <button className="help-arrow" onClick={nextImage}>➡</button>
+
+                    <button className="help-arrow" onClick={nextImage}>
+                        ➡
+                    </button>
                 </div>
-                <p>Step {current + 1} of {images.length}</p>
+
+                <p className="help-step">
+                    Step {current + 1} of {images.length}
+                </p>
             </div>
         </div>
     );
